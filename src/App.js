c72a642823744166dda22d2b01e93bf7c77be919
SBS세익 <seik1224@naver.com>
2025-07-13 10:38:10 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Step01 from './step/step01';
 import Step02 from './step/step02';
+import Step03 from './step/step03';
+import Step04 from './step/step04';
 
 function App() {
   return (
@@ -11,7 +13,9 @@ function App() {
       {/* <Comp></Comp>
       <Comp></Comp> */}
       {/* <Step01></Step01> */}
-      <Step02 />
+      {/* <Step02 /> */}
+      {/* <Step03 /> */}
+      <Step04 />
     </div>
     
   );
--- src/step/step03.js
+++ src/step/step03.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
 const Step03 = () => {
-  return (
-    <div>Step03</div>
-  )
+    const handleClick = () => {
+        alert('버튼 클릭함');
+    }
+    //  JS 작성법
+    return (
+        <>
+            {/* 
+                onClick = { 실행할함수 }
+                onClick = { function(){실행할코드} }
+                onClick = { () => {실행할코드}}
+            */}
+            <button onClick={()=>alert('버튼클릭함')}>클릭</button>    
+            <button onClick={handleClick}>클릭</button>    
+        </>
+    )
 }
 
 export default Step03
