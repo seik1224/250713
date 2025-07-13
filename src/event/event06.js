d03eb6b3dfc7f8d9c8e3a792f203056cbbd2d3bd
SBS세익 <seik1224@naver.com>
2025-07-13 12:04:20 +0900

    .

--- src/event/event06.js
+++ src/event/event06.js
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 // 폼 제출
 const Event06 = () => {
-  return (
-    <form>
-        <input type='text' value='' placeholder='값입력' />
-        <button type='submit'>제출</button>
-        <p>msg</p>
-    </form>
-  )
+    const [val, setVal] = useState('');
+    const [msg, setMsg] = useState('');
+
+    const handleSumbit = (e) => {
+        e.preventDefault();
+        setMsg(`제출된값 : ${val}`)
+    }
+    return (
+        <form onSubmit={handleSumbit}>
+            <input onChange={(e) => setVal(e.target.value)} type='text' value={val} placeholder='값입력' />
+            <button type='submit'>제출</button>
+            <p>{msg}</p>
+        </form>
+    )
 }
 
 export default Event06
