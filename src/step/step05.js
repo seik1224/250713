8f3ba7183a9fb15f760dbf9f2326851b7c72c371
SBS세익 <seik1224@naver.com>
2025-07-13 11:16:30 +0900

    .

--- src/step/step05.js
+++ src/step/step05.js
@@ -16,6 +16,10 @@ const Step05 = () => {
     // let [a,b] = [10,20]
     const [ count, setCount ] = useState(0);
 
+    const [ like, setLike ] =useState([0,0]);
+
+    console.log(like)
+    // const like = [0,0]  ->  [1,0]  
 
   return (
     <>
@@ -26,12 +30,16 @@ const Step05 = () => {
         {/* 배열에서 state값 변경 */}
         <div className='border border-red-500'>
             <p>게시물1</p>
-            <p>현재 좋아요 : 0</p>
-            <button>좋아요증가</button>
+            <p>현재 좋아요 : {like[0]}</p>
+            <button onClick={()=> {
+                const copy = [...like];
+                copy[0] = copy[0] + 1;
+                setLike(copy);
+            }}>좋아요증가</button>
         </div>
         <div className='border border-red-500'>
             <p>게시물2</p>
-            <p>현재 좋아요 : 0</p>
+            <p>현재 좋아요 : {like[1]}</p>
             <button>좋아요증가</button>
         </div>
     </>
---
+++ src/step/step06.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Step06 = () => {
+  return (
+    <div>Step06</div>
+  )
+}
+
+export default Step06
